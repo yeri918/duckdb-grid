a456ec503a078c3764e9c7598708d64ae58e4e4a
Dominic Leung <leungcheuk209@hotmail.com>
2024-07-21 16:03:43 +0800

    feat: init commit of folder structure

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import MyAgGrid from './components/grid/aggrid';
+
+import { useEffect, useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import logo from './logo.svg';
+import './App.css';
+import StdAgGrid from './components/grid/stdGrid';
+import ParquetAgGrid from './components/grid/parquetGrid';
+
+function App() {
+  return (
+    <div>
+      <StdAgGrid />
+    </div>
+  );
+}
+
+export default App;
