69c10c7980da926fd160049aea78f58777125cda
Dominic Leung <leungcheuk209@hotmail.com>
2024-07-29 00:46:55 +0800

    feat: add tsconfig json and try to debug on s3 parquet

--- app/src/components/table/initTable.tsx
+++ app/src/components/table/initTable.tsx
@@ -45,12 +45,42 @@ export const InitParquetTable = () => {
             */
             const src = new URL("./bankdataset.parquet", document.baseURI).href;
             const source = `
-                            CREATE VIEW bankdata AS
+                            CREATE OR REPLACE VIEW bankdata AS
                             FROM read_parquet('${src}')
                             SELECT *, row_number() over () as rn `;
             await c.query(source);
+            
+            
+            /*
+                S3 Parquet FIles
+
+                                CREATE SECRET secret1 (
+                    TYPE S3,
+                    KEY_ID '${import.meta.env.VITE_S3_ACCESS_KEY}',
+                    SECRET '${import.meta.env.VITE_S3_SECRET_ACCESS_KEY}',
+                    REGION 'ap-southeast-2'
+                );
+            */
+        //    TODO: THIS IS NOT WORKING
+        // region: NOT WORKING
+            const s3Login = `
+                INSTALL httpfs;
+                LOAD httpfs;
+            `;
+            await c.query(s3Login);
+
+            const taxiBucket = 'taxi';
+            const taxiPath = 'taxi_202403.parquet';
+            const taxiTableName = 'taxi';
+            const taxiQuery = `
+                            CREATE TABLE ${taxiTableName} AS
+                            SELECT *
+                            FROM 's3://${taxiBucket}/${taxiPath}';
+                            SELECT *, row_number() over () as rn `;
+            await c.query(taxiQuery);
             await c.close();
         };
+        // endregion
 
         initTable();
     }, []);
