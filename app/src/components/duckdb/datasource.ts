74864107aa9a04687c340a19719abce6573df529
yeri918 <61730858+yeri918@users.noreply.github.com>
2024-07-28 21:39:05 +0800

    feat: Execute a simple duckdb query in server side data source

--- app/src/components/duckdb/datasource.ts
+++ app/src/components/duckdb/datasource.ts
@@ -16,51 +16,38 @@ const createServerSideDatasource = (
   return {
     getRows: async (params: IServerSideGetRowsParams) => {
       console.log("Requesting rows", params.request);
+      // Construct the SQL query
+      const sql = `
+        WITH SOURCE AS (${source}),
+        FILTERED AS (
+            SELECT * FROM SOURCE
+        ),
+        GROUPFILTERED AS (
+            SELECT * FROM FILTERED
+        ),
+        QUERY AS (
+            SELECT * FROM GROUPFILTERED
+        )
+        
+        SELECT * FROM QUERY
+    `;
+
+      console.log("sql", sql);
+
+      // Make a DuckDB connection
+      const connection = await database.connect();
       try {
-        const data = fetchData();
-        const { rows, lastRow } = await data;
-        params.success({ rowData: rows });
-      } catch (error) {
+        // Execute the query and convert the result to an array of objects
+        const result = await connection.query(sql);
+        const rowData = result.toArray();
+        params.success({ rowData });
+      } catch {
         params.fail();
+      } finally {
+        await connection.close();
       }
     },
   };
 };
-
-interface RowData {
-  [key: string]: string | number;
-}
-
-async function fetchData() {
-  const response = await fetch("/bankdataset.csv");
-  if (!response.ok) {
-    throw new Error("Failed to fetch the CSV file.");
-  }
-  const reader = response.body?.getReader();
-  if (!reader) {
-    throw new Error("Failed to get the reader from the response body.");
-  }
-  const result = await reader.read(); // raw array
-  const decoder = new TextDecoder("utf-8");
-  const csv = decoder.decode(result.value); // the csv text
-  const results = Papa.parse(csv, {
-    header: true,
-    delimiter: ",",
-    transform: (value, header) => {
-      if (
-        header === "Value" ||
-        header === "Transaction_count" ||
-        header === "rn"
-      ) {
-        return parseFloat(value);
-      }
-      return value;
-    },
-  }); // object with { data, errors, meta }
-
-  const rows = results.data as RowData[]; // array of objects
-  const lastRow = rows.length;
-  return { rows, lastRow };
-}
 
 export default createServerSideDatasource;
