573c1fb8cd951cf40d40d6b02221b8a8b6ea0f1d
Dominic Leung <leungcheuk209@hotmail.com>
2024-08-01 00:02:28 +0800

    refactor: select.ts

--- app/src/components/duckGrid/sql_builder/select.ts
+++ app/src/components/duckGrid/sql_builder/select.ts
@@ -10,11 +10,7 @@ const buildSelect = async (
 ) => {
   const isGrouped = params.request?.rowGroupCols.length > 0;
 
-  // If no grouping, select all columns
-  if (!isGrouped) {
-    return "*";
-  }
-
+  
   const groupByKeys = params.request?.rowGroupCols
     .map((key) => key.field)
     .join(",");
@@ -28,26 +24,32 @@ const buildSelect = async (
         AND data_type IN ('INTEGER', 'BIGINT', 'SMALLINT', 'TINYINT', 'FLOAT', 'DOUBLE', 'DECIMAL');
 `;
 
-  // TODO: separate function to make the connection
-  const connection = await database.connect();
-  const result = await connection.query(sql);
-  await connection.close();
+  // If no grouping, select all columns
+  if (!isGrouped) {
+    return "*";
+  } else {
+    // TODO: separate function to make the connection
+    const connection = await database.connect();
+    const result = await connection.query(sql);
+    await connection.close();
 
-  const numericCols = JSON.parse(JSON.stringify(result.toArray())).map(
-    (col: { column_name: string }) => col.column_name
-  );
+    const numericCols = JSON.parse(JSON.stringify(result.toArray())).map(
+      (col: { column_name: string }) => col.column_name
+    );
 
-  // TODO: change the aggfunc when user specifies it
-  const valueCols = params.request?.valueCols;
-  if (valueCols) {
-    console.log("valueCols", valueCols);
-  }
-  console.log(numericCols, "numeric");
-  const aggCols = numericCols
-    .map((col: string) => `SUM(${col}) AS ${col}`)
-    .join(", ");
+    // TODO: change the aggfunc when user specifies it
+    const valueCols = params.request?.valueCols;
+    if (valueCols) {
+      console.log("valueCols", valueCols);
+    }
+    console.log(numericCols, "numeric");
+    const aggCols = numericCols
+      .map((col: string) => `SUM(${col}) AS ${col}`)
+      .join(", ");
 
-  return `${groupByKeys}, ${aggCols}`;
+    return `${groupByKeys}, ${aggCols}`;
+  };
+  
 };
 
 export default buildSelect;
