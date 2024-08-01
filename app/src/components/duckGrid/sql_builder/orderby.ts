51ee6394b4dd29da278860d38f3b72542d31e3b7
Dominic Leung <leungcheuk209@hotmail.com>
2024-08-02 00:08:34 +0800

    chore: format on save

--- app/src/components/duckGrid/sql_builder/orderby.ts
+++ app/src/components/duckGrid/sql_builder/orderby.ts
@@ -5,62 +5,59 @@
 //
 
 import {
-    IServerSideDatasource,
-    IServerSideGetRowsParams,
-    ResizableStructure,
+  IServerSideDatasource,
+  IServerSideGetRowsParams,
+  ResizableStructure,
 } from "ag-grid-community";
 import { AsyncDuckDB } from "@duckdb/duckdb-wasm";
 
 const buildOrderBy = async (
-    database: AsyncDuckDB,
-    params: IServerSideGetRowsParams,
+  database: AsyncDuckDB,
+  params: IServerSideGetRowsParams,
 ) => {
+  const rowGroupCols = params.request?.rowGroupCols;
+  const groupKeys = params.request?.groupKeys;
+  let sortModel = params.request?.sortModel;
 
-    const rowGroupCols = params.request?.rowGroupCols;
-    const groupKeys = params.request?.groupKeys;
-    let sortModel = params.request?.sortModel;
+  let eligSortParts: string[] = [];
 
-    let eligSortParts: string[] = [];
+  // Handle empty sortModel case.
+  if (typeof sortModel === "undefined" || sortModel.length === 0) {
+    return "";
+  }
 
-    // Handle empty sortModel case.
-    if (typeof sortModel === "undefined" || sortModel.length === 0) {
-        return "";
-    };
+  if (typeof rowGroupCols === "undefined" || rowGroupCols.length === 0) {
+    // No Group By Case
+    sortModel.forEach((key) => {
+      eligSortParts.push(`${key.colId} ${key.sort}`);
+    });
+  } else {
+    // Handle Group By Case.
+    let rowGroupColIds = rowGroupCols.map((col) => col.id);
+    let valueColIds = params.request?.valueCols?.map((col) => col.id);
+    let allCols = rowGroupColIds?.concat(valueColIds);
 
-    if (typeof rowGroupCols === "undefined" || rowGroupCols.length === 0) {
-        // No Group By Case
-        sortModel.forEach((key) => {
-            eligSortParts.push(`${key.colId} ${key.sort}`);
-        });
-        
-    } else {
-        // Handle Group By Case.
-        let rowGroupColIds = rowGroupCols.map((col) => col.id);
-        let valueColIds = params.request?.valueCols?.map((col) => col.id);
-        let allCols = rowGroupColIds?.concat(valueColIds);
+    let intersectCols = sortModel?.filter((value) =>
+      allCols.includes(value.colId),
+    );
 
-        let intersectCols = sortModel?.filter((value) =>
-            allCols.includes(value.colId),
-        );
+    intersectCols.forEach((key) => {
+      let colId = key.colId;
+      if (colId === "Auto-Grid-Column") {
+        let autoGroupOrder = rowGroupColIds
+          .map((colId) => {
+            return colId + " " + key.sort;
+          })
+          .join(",");
+        eligSortParts.push(autoGroupOrder);
+      } else {
+        eligSortParts.push(colId + " " + key.sort);
+      }
+    });
+  }
+  console.log("Eligible Sort Parts: ", eligSortParts);
 
-        intersectCols.forEach((key) => {
-            let colId = key.colId;
-            if (colId === "Auto-Grid-Column") {
-                let autoGroupOrder = rowGroupColIds
-                    .map((colId) => {
-                        return colId + " " + key.sort;
-                    })
-                    .join(",");
-                eligSortParts.push(autoGroupOrder);
-            } else {
-                eligSortParts.push(colId + " " + key.sort);
-            }
-        });
-    };
-    console.log("Eligible Sort Parts: ", eligSortParts);
-
-    return `ORDER BY ${eligSortParts.join(", ")}`;
-
+  return `ORDER BY ${eligSortParts.join(", ")}`;
 };
 
 export default buildOrderBy;
