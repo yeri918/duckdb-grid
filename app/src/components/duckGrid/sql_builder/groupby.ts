a7a0ff2607bcd78b062dd54f74deb20eda5eb23e
yeri918 <61730858+yeri918@users.noreply.github.com>
2024-08-02 00:29:16 +0800

    feat: Improve group by logic in duckGrid component to handle when groupKeys and rowGroupCols are the same

--- app/src/components/duckGrid/sql_builder/groupby.ts
+++ app/src/components/duckGrid/sql_builder/groupby.ts
@@ -6,10 +6,13 @@ import { AsyncDuckDB } from "@duckdb/duckdb-wasm";
 
 const buildGroupBy = async (
   database: AsyncDuckDB,
-  params: IServerSideGetRowsParams
+  params: IServerSideGetRowsParams,
 ) => {
   const isGrouped = params.request?.rowGroupCols.length > 0;
-  if (!isGrouped) {
+  const isFullyOpened =
+    params.request?.groupKeys.length > 0 &&
+    params.request?.groupKeys.length === params.request?.rowGroupCols.length;
+  if (!isGrouped || isFullyOpened) {
     return "";
   }
   const groupByKeys = params.request?.rowGroupCols
--- app/src/components/duckGrid/sql_builder/limit.ts
+++ app/src/components/duckGrid/sql_builder/limit.ts
@@ -1,19 +1,19 @@
 import {
-    IServerSideDatasource,
-    IServerSideGetRowsParams,
+  IServerSideDatasource,
+  IServerSideGetRowsParams,
 } from "ag-grid-community";
 import { AsyncDuckDB } from "@duckdb/duckdb-wasm";
 
 const buildLimit = async (
-    database: AsyncDuckDB,
-    params: IServerSideGetRowsParams,
+  database: AsyncDuckDB,
+  params: IServerSideGetRowsParams,
 ) => {
-    const startRow =
-        params.request.startRow !== undefined ? params.request.startRow : 0;
-    const endRow =
-        params.request.endRow !== undefined ? params.request.endRow : 100;
-    const pageSize = endRow - startRow;
-    return " LIMIT " + (pageSize + 1) + " OFFSET " + startRow;
+  const startRow =
+    params.request?.startRow !== undefined ? params.request.startRow : 0;
+  const endRow =
+    params.request?.endRow !== undefined ? params.request.endRow : 100;
+  const pageSize = endRow - startRow;
+  return " LIMIT " + (pageSize + 1) + " OFFSET " + startRow;
 };
 
 export default buildLimit;
