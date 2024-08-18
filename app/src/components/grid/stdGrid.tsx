182380a580512a4eae02e08bef2626a08b874872
Dominic Leung <leungcheuk209@hotmail.com>
2024-08-18 11:14:08 -0700

    feat: add duckStatusBar

--- app/src/components/grid/stdGrid.tsx
+++ app/src/components/grid/stdGrid.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState, useMemo, useRef } from "react";
-import { ColumnDataType, RowData, ColumnDef } from './gridTypes';
+import { ColumnDataType, RowData, ColumnDef, CountStatusBarComponentType } from './gridTypes';
+import CountStatusBarComponent from '../duckGrid/duckStatusBar';
 import { AgGridReact } from "ag-grid-react";
+import { ColDef, StatusPanelDef } from '@ag-grid-community/core';
+import { Grid, GridApi } from "ag-grid-enterprise";
 import "ag-grid-enterprise";
 import './style.css';
 import "ag-grid-community/styles/ag-grid.css";
@@ -8,8 +11,6 @@ import "ag-grid-community/styles/ag-theme-alpine.css";
 import duckGridDataSource from "../duckGrid/duckGridDS";
 import db from "../table/duckDB";
 import { getColumnDefs, getLayeredColumnDefs, getGroupedColumnDefs } from './gridHelper'
-
-
 
 interface StdAgGridProps {
   columnDataType: ColumnDataType;
@@ -52,7 +53,10 @@ const StdAgGrid: React.FC<StdAgGridProps> = (props) => {
   const onModelUpdated = (params: any) => {
   };
 
+
+
   const onGridReady = (params: any) => {
+    setGridApi(params.api);
   };
 
   const onFirstDataRendered = () => {
@@ -62,6 +66,83 @@ const StdAgGrid: React.FC<StdAgGridProps> = (props) => {
       props.setExecutionTime(execTime);
     }
   };
+
+  const getContextMenuItems = (params: any) => {
+    return [
+      // ...any other menu items you want to include
+      {
+        name: "Filters",
+        subMenu: [
+          {
+            name: "Option 1",
+            action: () => {
+              // Code for Option 1
+            }
+          },
+          {
+            name: "Option 2",
+            action: () => {
+              // Code for Option 2
+            }
+          },
+          {
+            name: "Option 3",
+            action: () => {
+              // Code for Option 3
+            }
+          }
+        ],
+      },
+      "separator",
+      {
+        name: "Groups",
+        subMenu: [
+          {
+            name: "Option 1",
+            action: () => {
+              // Code for Option 1
+              gridApi?.collapseAll();
+
+            }
+          },
+          {
+            name: "Option 2",
+            action: () => {
+              // Code for Option 2
+            }
+          },
+          {
+            name: "Option 3",
+            action: () => {
+              // Code for Option 3
+            }
+          }
+        ],
+      },
+      "separator",
+      "copy",
+      "export",
+    ];
+  };
+
+  const statusBar = useMemo<{
+    statusPanels: StatusPanelDef[];
+  }>(() => {
+    return {
+      statusPanels: [
+        {
+          statusPanel: (props: CountStatusBarComponentType<any, any>) => <CountStatusBarComponent context={undefined} {...props} {...db} />,
+        },
+        {
+          statusPanel: 'agAggregationComponent',
+          statusPanelParams: {
+            aggFuncs: ['count', 'sum'],
+          },
+        },
+      ],
+    };
+  }, []);
+
 
   return (
     <div >
@@ -75,6 +156,7 @@ const StdAgGrid: React.FC<StdAgGridProps> = (props) => {
             columnDefs={columnDefs}
             defaultColDef={defaultColDef}
             autoGroupColumnDef={autoGroupColumnDef}
+            getContextMenuItems={getContextMenuItems}
             multiSortKey={"ctrl"}
             sideBar={true}
             serverSidePivotResultFieldSeparator="_"
@@ -84,6 +166,12 @@ const StdAgGrid: React.FC<StdAgGridProps> = (props) => {
             onFirstDataRendered={onFirstDataRendered}
             rowHeight={25}
             suppressMultiSort={false}
+            // Multiple selection
+            enableRangeSelection={true}
+            rowSelection="multiple"
+            suppressRowClickSelection={true}
+            // StatusBar
+            statusBar={statusBar}
           />
         </div>
       </div>
