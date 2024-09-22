1436eaeef3bb36aebf59678308a924f6c5baf512
Dominic Leung <leungcheuk209@hotmail.com>
2024-09-22 23:34:08 +0800

    chore: update eslint config

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -1,21 +1,40 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,  // Enable browser global variable
+    es2020: true,   // Enable ES2020 syntax
+    node: true,     // Enable Node.js global variables & Node.js scoping
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended', // TypeScipt rules
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ['dist', 'node_modules'],
+  parser: '@typescript-eslint/parser', // TypeScipt rules
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the React version
+    },
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint', // TypeScipt rules
+  ],
   rules: {
     'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-hooks/rules-of-hooks': 'error', 
+    'react-hooks/exhaustive-deps': 'warn', 
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // TypeScipt rules
   },
 }
--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,5 +1,4 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
@@ -7,7 +6,6 @@ import pluginReact from "eslint-plugin-react";
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
