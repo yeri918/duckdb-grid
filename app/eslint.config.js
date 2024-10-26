e1fa1a6a5d07b96bc5e7da32f7a734029ff73c11
Dominic Leung <leungcheuk209@hotmail.com>
2024-10-26 11:02:05 +0800

    add eslint config and more comments descriptions

---
+++ app/eslint.config.js
@@ -0,0 +1,26 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},        // rules applies to all matching files.
+  {languageOptions: { globals: globals.browser }},  // extends the global variables available in the linting environment 
+                                                    //      to include those specific to a browser environment
+  pluginJs.configs.recommended,                     // default js rules
+  ...tseslint.configs.recommended,                  // default ts rules
+  pluginReact.configs.flat.recommended,
+
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+    },
+  },
+
+  {
+    ignores: [".node_modules/*"]
+  },
+];
