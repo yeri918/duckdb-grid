51a43c1ec45c7b3b67566f3254f089ccf67538dc
Dominic Leung <leungcheuk209@hotmail.com>
2024-10-27 15:34:53 +0800

    auto update date column to YYYY-mm-dd

--- app/src/lib/load/createTable.ts
+++ app/src/lib/load/createTable.ts
@@ -12,5 +12,21 @@ export async function createTable(registeredName: string, tableName: string) {
         CREATE OR REPLACE TABLE ${tableName} AS
         SELECT * FROM '${registeredName}'
         `);
+
+  const loadedColumns = await c.query(`Describe ${tableName}`);
+  const result = loadedColumns.toArray().map((row) => row.toJSON());
+  console.log("leudom describe", result);
+
+  await result.forEach((column) => {
+    if (column.column_type === "DATE") {
+      c.query(`
+          -- https://duckdb.org/docs/sql/statements/alter_table.html#examples
+          ALTER TABLE ${tableName} ALTER ${column.column_name} 
+          SET DATA TYPE VARCHAR USING strftime(${column.column_name}, '%Y-%m-%d');
+        `);
+      console.log("leudom describe alter", column);
+    }
+  });
+
   await c.close();
 }
