4063bf24643eff4d2c8d952dea0dbbb61d6ed063
Dominic Leung <leungcheuk209@hotmail.com>
2024-10-28 21:34:50 +0800

    update lint config

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -3,27 +3,11 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-// eslint.config.mjs
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},        // rules applies to all matching files.
-  {languageOptions: { globals: globals.browser }},  // extends the global variables available in the linting environment 
-                                                    //      to include those specific to a browser environment
-  pluginJs.configs.recommended,                     // default js rules
-  ...tseslint.configs.recommended,                  // default ts rules
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  eslintPluginPrettierRecommended,
-
-  {
-    rules: {
-      eqeqeq: "off",
-      "no-unused-vars": "error",
-      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
-    },
-  },
-
-  {
-    ignores: [".node_modules/*"]
-  },
 ];
